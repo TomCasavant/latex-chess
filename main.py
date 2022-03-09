b2e7467aedd58c6942c6e12a99db4eddec6bacbc
Tom Casavant <tfcasavant@gmail.com>
2022-03-09 18:23:05 -0500

    Added random move generator

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import random
+
 import pylatex
 from pylatex import NoEscape
 import chess
+import sys
 
 class ChessLaTeX:
     def __init__(self, moves):
         self.moves = moves
-        self.fen = self.generate_fen()
+        self.game = self.get_game()
+        self.fen = self.game.fen()
         self.is_valid = self.fen is not False
 
     def draw_chessboard(self):
@@ -23,7 +27,7 @@ class ChessLaTeX:
         doc.generate_pdf('chessboard_display', compiler='pdflatex')
 
     # Generate FEN notation from list of moves
-    def generate_fen(self):
+    def get_game(self):
         # Create a new game and play moves
         game = chess.Board()
         for move in self.moves:
@@ -32,14 +36,32 @@ class ChessLaTeX:
             except ValueError:
                 return False
         # Return FEN notation
-        return game.fen()
+        return game
+
+    # Get list of valid moves, and choose a random one, save to the csv file
+    def make_random_move(self):
+        # Get the list of valid moves from this game
+        # Convert generator to list
+        legal_moves = self.game.legal_moves
+        # Choose a random move from the list
+        move = random.choice(list(legal_moves))
+        # Save the move to the csv file
+        with open('games/current_game.csv', 'a') as f:
+            f.write(f"\n{move}")
+
 
 if __name__ == "__main__":
     # Read in games/current_game.csv (which contains one column of moves)
+    # Get argument from command line
+    # If argument is 'move' then generate a random move
+    # If argument is 'generate' then draw the chessboard
     with open('games/current_game.csv', 'r') as f:
         moves = f.read().splitlines()
-    # Create ChessLaTeX object
     chess_latex = ChessLaTeX(moves)
-    chess_latex.draw_chessboard()
+    choice = sys.argv[1]
+    if choice == 'move':
+        chess_latex.make_random_move()
+    else:
+        chess_latex.draw_chessboard()
 
 
